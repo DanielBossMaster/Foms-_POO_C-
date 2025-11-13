7796d4e901bc7591739d968248d483c09e6dfe9c
DanielBossMaster <sena.danielboss@gmail.com>
2025-11-13 16:22:05 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foms__POO_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
